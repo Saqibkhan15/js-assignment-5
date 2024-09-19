e007e5ba90aa9cce15eef23fbbc7555cbe8ccf7f
Saqibkhan15 <aqib12u@gmail.com>
2024-09-19 09:15:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+
+// // 1
+// var empty = []
+
+
+
+
+// // 2 
+// var empty = new Array()
+
+
+
+// // 3
+
+
+// var perfume = ["zellbury", "j.", "rassasi"]
+
+
+// // 4
+
+
+// var numbers = [1, 2, 3]
+
+
+
+// // 5
+
+// var answers = [true, false]
+
+// // 6
+
+
+
+// var personalData = ["Saqib", 26, true]
+ // 7
+
+// var qualifications = [
+//     "SSC",
+//     "HSC",
+//     "BCS",
+//     "BS",
+//     "BCOM",
+//     "MS",
+//     "M. Phil.",
+//     "PhD"
+//   ];
+  
+//   document.write("<h1>Qualifications</h1>");
+//   document.write("1. " + qualifications[0] + "<br>");
+//   document.write("2. " + qualifications[1] + "<br>");
+//   document.write("3. " + qualifications[2] + "<br>");
+//   document.write("4. " + qualifications[3] + "<br>");
+//   document.write("5. " + qualifications[4] + "<br>");
+//   document.write("6. " + qualifications[5] + "<br>");
+//   document.write("7. " + qualifications[6] + "<br>");
+//   document.write("8. " + qualifications[7] + "<br>");
+  
+  
